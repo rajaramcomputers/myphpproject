2d8588d1374f290a04db4cf331b781b0f78f252c
rajaramcomputers <rajaramcomputers@gmail.com>
2024-04-17 17:01:15 +0400

    mysql

---
+++ classmysql/mysqltest.php
@@ -0,0 +1,17 @@
+
+<?php
+// Open and close a connection to MySQL MySQLi - Procedural
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
